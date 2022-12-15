2dfa91dc499a1e1045b7eca92a48c56e2d803384
kshammer <kshammerschmidt@gmail.com>
2022-12-15 00:17:03 -0800

    3.2

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iter_array_chunks)]
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
@@ -7,25 +8,24 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
     let mut score = 0;
-    for line in reader.lines() {
-        let cool_str = line.unwrap();
-        let first_half: HashSet<char> = cool_str.chars().take(cool_str.len() / 2).collect();
-        let second_half: String = cool_str
-            .chars()
-            .skip(cool_str.len() / 2)
-            .take(cool_str.len())
-            .collect();
-        second_half.chars().any(|c| {
-            if first_half.contains(&c) {
-                let val = if c.is_lowercase() {
-                    c as i32 - 96
-                } else {
-                    c as i32 - 38
-                };
-                score = score + val;
-            }
-            first_half.contains(&c)
-        });
+    for [first, second, third] in reader.lines().array_chunks() { 
+        let first_hash: HashSet<char> = first.unwrap().chars().collect();
+        let second_hash: HashSet<char> = second.unwrap().chars().collect();
+        let third_hash: HashSet<char> = third.unwrap().chars().collect();
+        let first_inter:HashSet<char> = first_hash.intersection(&second_hash).cloned().collect();
+        let second_inter:HashSet<char> = first_inter.intersection(&third_hash).cloned().collect();
+        let cool = second_inter.iter().next().unwrap().clone();
+        let val = if cool.is_lowercase() {cool as i32 - 96} else {cool as i32 -38};
+        score = score + val; 
     }
     println!("Score {}", score);
 }
+
+
+//let val = if c.is_lowercase() {
+//                    c as i32 - 96
+ //               } else {
+  //                  c as i32 - 38
+   //             };
+    //            score = score + val;
+
