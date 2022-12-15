47466deafe7c72384787639f0f811e02ac551b4f
kshammer <kshammerschmidt@gmail.com>
2022-12-14 23:39:46 -0800

    cleaned up code

--- three/src/main.rs
+++ three/src/main.rs
@@ -15,16 +15,17 @@ fn main() {
             .skip(cool_str.len() / 2)
             .take(cool_str.len())
             .collect();
-        let mut inter_char = 'a';
         second_half.chars().any(|c| {
             if first_half.contains(&c) {
-                inter_char = c;
+                let val = if c.is_lowercase() {
+                    c as i32 - 96
+                } else {
+                    c as i32 - 38
+                };
+                score = score + val;
             }
             first_half.contains(&c)
         });
-        let val = if inter_char.is_lowercase() { inter_char as i32 - 96} else { inter_char as i32 - 38 };
-        println!("Cool {}, {}", inter_char, val);
-        score = score + val; 
     }
     println!("Score {}", score);
 }
